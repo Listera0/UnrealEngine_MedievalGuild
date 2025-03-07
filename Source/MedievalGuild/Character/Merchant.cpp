12a3c361c1eb6ce5290c4ae78fa608ce74e460c0
suyeon216 <cykes216106@gmail.com>
2025-03-07 17:40:41 +0900

    Fix ItemData
    ItemData 저장위치 및 구조 변경

--- Source/MedievalGuild/Character/Merchant.cpp
+++ Source/MedievalGuild/Character/Merchant.cpp
@@ -22,30 +22,6 @@ void AMerchant::BeginPlay()
     Super::BeginPlay();
 
     UItemDataManager* ItemDataManager = UItemDataManager::GetInstance();
-
-
-    UItemData* NewItem = NewObject<UItemData>();
-    NewItem->name = TEXT("Shield");
-    NewItem->description = TEXT("A protective shield.");
-    NewItem->price = 75.0f;
-    NewItem->index = 2;
-    NewItem->count = 1;
-    NewItem->height = 3;
-    NewItem->weight = 6;
-    NewItem->eItemType = EItemType::Clothes;
-
-    ItemDataManager->AddItemData(NewItem);
-
-    FString FilePath = FPaths::ProjectSavedDir() / TEXT("itemData.json");
-    ItemDataManager->SaveAllItemDataToJson(FilePath);
-    GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Yellow, TEXT("ItemData Save"));
-
-    ItemDataManager->LoadAllItemDataFromJson(FilePath);
-    GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Yellow, TEXT("ItemData Load"));
-    for (UItemData* LoadedItem : ItemDataManager->GetItemDataList())
-    {
-        UE_LOG(LogTemp, Log, TEXT("Loaded Item: %s"), *LoadedItem->name);
-    }
 }
 
 void AMerchant::Tick(float DeltaTime)
@@ -53,46 +29,22 @@ void AMerchant::Tick(float DeltaTime)
     Super::Tick(DeltaTime);
 }
 
-AItem_Base* AMerchant::SellItem(int index, int Count)
+
+UItemData* AMerchant::SellItem(int index, int Count)
 {
-    if (ItemInstances.Num() != Item_List.Num())
+    for (UItemData* FoundItem : ItemDatas)
     {
-        ItemInstances.Empty();
-        GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Yellow, TEXT("ItemInstances size does not match Item_List size. Re-spawning items."));
-
-        for (TSubclassOf<AItem_Base> ItemClass : Item_List)
+        if (FoundItem && FoundItem->index == index)
         {
-            AItem_Base* NewItem = GetWorld()->SpawnActor<AItem_Base>(ItemClass);
-            if (NewItem)
+            if (true)
             {
-                NewItem->SetActorHiddenInGame(true);
-                ItemInstances.Add(NewItem);
-                GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, FString::Printf(TEXT("Spawned new item of class: %s"), *ItemClass->GetName()));
-            }
-        }
-    }
+                GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, FString::Printf(TEXT("Item sold successfully. Remaining count: %d"), Count));
 
-    for (AItem_Base* FoundItem : ItemInstances)
-    {
-        if (FoundItem && FoundItem->GetIndex() == index)
-        {
-            if (FoundItem->Sell(Count))
-            {
-                GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, FString::Printf(TEXT("Item sold successfully. Remaining count: %d"), FoundItem->GetCount()));
 
-                AItem_Base* NewItem = GetWorld()->SpawnActor<AItem_Base>(FoundItem->GetClass());
-                if (NewItem)
-                {
-                    NewItem->SetCount(Count);
-                    NewItem->SetActorHiddenInGame(true);
-                    GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, FString::Printf(TEXT("Create Item. Item count: %d"), NewItem->GetCount()));
-                    return NewItem;
-                }
-                else
-                {
-                    GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("Failed to spawn new item."));
-                    return nullptr;
-                }
+
+                UItemData* SoldItem = NewObject<UItemData>();
+
+                return SoldItem;
             }
             else
             {
@@ -101,7 +53,6 @@ AItem_Base* AMerchant::SellItem(int index, int Count)
             }
         }
     }
-
     GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, TEXT("Item not found in merchant's inventory!"));
     return nullptr;
 }
--- Source/MedievalGuild/Item/ItemData.h
+++ Source/MedievalGuild/Item/ItemData.h
@@ -11,14 +11,6 @@ class MEDIEVALGUILD_API UItemData : public UDataAsset
 	GENERATED_BODY()
 
 public:
-	// 아이템 데이터를 JSON으로 저장하는 함수
-	UFUNCTION(BlueprintCallable, Category = "Item Data")
-	void SaveItemDataToJson(const FString& FilePath);
-
-	// JSON 파일에서 아이템 데이터를 불러오는 함수
-	UFUNCTION(BlueprintCallable, Category = "Item Data")
-	static UItemData* LoadItemDataFromJson(const FString& FilePath);
-
 	// 아이템 타입
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
 	EItemType eItemType;
@@ -39,11 +31,11 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
 	int index;
 
-	// 아이템 개수
+	// 아이템 넓이
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
-	int count;
+	int width;
 
-	// 아이템 크기
+	// 아이템 높이
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Data")
 	int height;
 
